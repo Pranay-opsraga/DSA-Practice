e9a9d947fd7053a084939200be09f335656e1a57
Pranay Sharma <Pranay Sharma>
2025-08-13 10:58:54 +0530

    Checked if array is sorted

--- Arrays/findslargest.cpp
+++ Arrays/findslargest.cpp
@@ -1,11 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 class Solution {
 public:
       int sle (vector<int>&nums){
         int largest = nums[0];
         int slargest = -1;
         for ( int i = 0 ; i < nums.size() ; i++){
-            if ( largest < nums[i] ) largest = nums[i];
-        
+            if ( largest < nums[i] ) {
+              slargest = largest ;
+            largest = nums[i];
+           
+            }
         if ( nums[i] < largest && nums[i] > slargest) slargest = nums[i];
         }
         return slargest;
@@ -15,3 +21,15 @@ public:
        return sle(nums);
     }
 };
+int main (){
+    int n ;
+    cin>>n ;
+    vector<int>arr(n);
+     for ( int i =0 ; i < n ; i++){
+        cin>>arr[i];
+     }
+     Solution obj;
+   int result =  obj.secondLargestElement(arr);
+   cout<<result;
+return 0;
+}
