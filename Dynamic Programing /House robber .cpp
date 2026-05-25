8a7ed6a1e04be02c41e4a0416bfa70cd323b2349
Pranay Sharma <sharmapranay1210@gmail.com>
2026-05-25 18:28:41 +0530

    Add tabulation solution for House Robber problem
    Implemented tabulation method for the House Robber problem.

--- Dynamic Programing /House robber .cpp
+++ Dynamic Programing /House robber .cpp
@@ -38,3 +38,28 @@ public:
         return prev1;
     }
 };
+
+// Tabulation
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+         vector<int>dp(n);
+
+         if(n == 1) return nums[0];
+
+         dp[0] = nums[0];
+         dp[1] = max(nums[0] , nums[1]);
+
+         for(int i = 2 ; i < n ; i++){
+            int pick = dp[i-2] + nums[i];
+            int notpick = dp[i-1];
+
+            int curr = max(pick , notpick);
+
+            dp[i] = curr;
+         }
+         return dp[n-1];
+    }
+};
