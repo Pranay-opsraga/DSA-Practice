9ebdf3f013c029b0d8ea3eecfd7b10cad903f75d
Pranay Sharma <sharmapranay1210@gmail.com>
2026-05-25 16:22:28 +0530

    Implement memoization and tabulation for climbing stairs
    Added memoization and tabulation approaches for climbing stairs problem.

--- Dynamic Programing /Climbing stairs .cpp
+++ Dynamic Programing /Climbing stairs .cpp
@@ -12,3 +12,46 @@ public:
         return prev1;
     }
 };
+
+// Memoization
+
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
+
+// Tabulation
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+
+        int prev1 = 2;
+        int prev2 = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
