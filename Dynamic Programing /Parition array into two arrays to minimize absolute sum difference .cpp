abe4056e2425ac452a9b684b56731b97817b8554
Pranay Sharma <sharmapranay1210@gmail.com>
2026-05-29 18:44:25 +0530

    Implement minDifference using tabulation and space optimization

--- Dynamic Programing /Parition array into two arrays to minimize absolute sum difference .cpp
+++ Dynamic Programing /Parition array into two arrays to minimize absolute sum difference .cpp
@@ -33,3 +33,76 @@ public:
 
 // Tabulation
 
+class Solution {
+  public:
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        int total = accumulate(arr.begin() , arr.end() , 0);
+        vector<vector<bool>>dp(n , vector<bool>(total+1 , false));
+        
+        for(int i = 0 ; i < n ; i++){
+             dp[i][0] = true;
+        }
+        
+        dp[0][arr[0]] = true;
+        
+        for(int i = 1 ; i < n ; i++){
+            for(int target = 1 ; target <= total ; target++){
+                  
+                  bool nottake = dp[i-1][target];
+                  bool take = false;
+                  if(arr[i] <= target) take = dp[i-1][target-arr[i]];
+                  
+                  dp[i][target] = nottake | take;
+            }
+        }
+        int ans = INT_MAX;
+        for(int s1 = 0 ; s1 <= total/2 ; s1++){
+             if(dp[n-1][s1]){
+                 ans = min(ans , abs((total - s1) - s1));
+             }
+        }
+        
+        return ans;
+    }
+};
+
+
+// Space optimization
+
+class Solution {
+  public:
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        int total = accumulate(arr.begin() , arr.end() , 0);
+        vector<bool>curr(total+1 , false);
+        vector<bool>prev(total+1 , false);
+        
+        
+            prev[0] = curr[0] = true;
+        
+            prev[arr[0]] = true;
+        
+        for(int i = 1 ; i < n ; i++){
+            for(int target = 1 ; target <= total ; target++){
+                  
+                  bool nottake = prev[target];
+                  bool take = false;
+                  if(arr[i] <= target) take = prev[target-arr[i]];
+                  
+                  curr[target] = nottake | take;
+            }
+            prev = curr;
+        }
+        int ans = INT_MAX;
+        for(int s1 = 0 ; s1 <= total/2 ; s1++){
+             if(prev[s1]){
+                 ans = min(ans , abs((total - s1) - s1));
+             }
+        }
+        
+        return ans;
+    }
+};
+
+
