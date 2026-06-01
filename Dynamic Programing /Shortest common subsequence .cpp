0de8b06894ed84f3545bfb01ebdfd3191a5bd03a
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-01 18:21:43 +0530

    Implement shortest common supersequence with memoization and tabulation

---
+++ Dynamic Programing /Shortest common subsequence .cpp
@@ -0,0 +1,119 @@
+// Memoization
+
+class Solution {
+public:
+    int solve(int i , int j , string str1, string str2 , vector<vector<int>>&dp){
+             
+              if(i == 0 || j == 0) return 0;
+
+              if(dp[i][j] != -1) return dp[i][j];
+
+              if(str1[i-1] == str2[j-1]) return dp[i][j] = 1 + solve(i-1, j-1 , str1 , str2 , dp);
+
+              return dp[i][j] = max(solve(i-1 , j , str1 , str2 , dp) , solve(i , j-1 , str1 , str2 , dp));
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+          
+          int n = str1.size();
+          int m = str2.size();
+          vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+          solve(n , m, str1, str2 , dp);
+
+          string ans = "";
+          int i = n;
+          int j = m;
+
+          while(i > 0 && j > 0){
+              
+              if(str1[i-1] == str2[j-1]){
+                   ans += str1[i-1];
+                   i--;
+                   j--;
+              } else if (dp[i-1][j] > dp[i][j-1]){
+
+                 ans += str1[i-1];
+                 i--;
+              }
+
+              else {
+                 ans +=str2[j-1];
+                 j--;
+              }
+          }
+
+          while(i > 0){
+            ans += str1[i-1];
+            i--;
+          }
+          while(j > 0){
+            ans += str2[j-1];
+            j--;
+          }
+          
+          reverse(ans.begin() , ans.end());
+          return ans;
+
+    }
+};
+
+// Tabulation
+
+class Solution {
+public:
+
+    string shortestCommonSupersequence(string str1, string str2) {
+          
+          int n = str1.size();
+          int m = str2.size();
+          
+          vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j],
+                                   dp[i][j - 1]);
+                }
+            }
+        }
+
+          string ans = "";
+          int i = n;
+          int j = m;
+
+          while(i > 0 && j > 0){
+              
+              if(str1[i-1] == str2[j-1]){
+                   ans += str1[i-1];
+                   i--;
+                   j--;
+              } else if (dp[i-1][j] > dp[i][j-1]){
+
+                 ans += str1[i-1];
+                 i--;
+              }
+
+              else {
+                 ans +=str2[j-1];
+                 j--;
+              }
+          }
+
+          while(i > 0){
+            ans += str1[i-1];
+            i--;
+          }
+          while(j > 0){
+            ans += str2[j-1];
+            j--;
+          }
+          
+          reverse(ans.begin() , ans.end());
+          return ans;
+
+    }
+};
