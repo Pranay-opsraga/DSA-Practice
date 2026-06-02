523dd05ad01b4304cdb61991b0dff5f9bdc7cd12
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-02 22:40:10 +0530

    Implement solution for stock buy and sell problem

---
+++ Dynamic Programing /Best time to buy and sell stocks .cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ans = 0;
+        int mn = prices[0];
+        for(int i = 0 ; i < n ; i++){
+            
+                   ans = max(ans , prices[i] - mn);
+                   mn = min(mn , prices[i]);
+        }
+        return ans;
+    }
+};
