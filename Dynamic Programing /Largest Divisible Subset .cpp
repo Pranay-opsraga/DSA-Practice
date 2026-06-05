cebbde243834f5988665a209d80110d24528d91e
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-05 18:14:21 +0530

    Add Largest Divisible Subset solution
    Implement Largest Divisible Subset algorithm in C++.

---
+++ Dynamic Programing /Largest Divisible Subset .cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+         
+         int n = nums.size();
+         sort(nums.begin() , nums.end());
+         vector<int>dp(n , 1);
+         vector<int>parent(n);
+         int maxi = 1;
+         int lastIndex = 0;
+         for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
+             for(int prev = 0 ; prev < i ; prev++){
+                  
+                  if((nums[i] % nums[prev]) == 0 && dp[prev] + 1 > dp[i]){
+                      dp[i] = dp[prev]+1;
+                      parent[i] = prev;
+                  }
+             }
+
+             if(dp[i] > maxi){
+                maxi = dp[i];
+                lastIndex = i;
+             }
+         }
+        vector<int>temp;
+         while(parent[lastIndex] != lastIndex){
+                temp.push_back(nums[lastIndex]);
+                lastIndex = parent[lastIndex];
+         }
+         temp.push_back(nums[lastIndex]);
+
+         reverse(temp.begin() , temp.end());
+         return temp;
+
+    }
+};
