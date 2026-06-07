108d2e53b346ed782e63b9499e966e17980024e4
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-07 19:09:27 +0530

    Add solution for Minimum Cost to Cut a Stick problem
    Implemented a solution for the Minimum Cost to Cut a Stick problem using memoization and tabulation techniques.

---
+++ Dynamic Programing /Minimum Cost to Cut a Stick  .cpp
@@ -0,0 +1,32 @@
+// Memoization
+
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int mini = 1e9;
+        for (int k = i; k <= j; k++) {
+            int cost = (cuts[j + 1] - cuts[i - 1]) + solve(i, k - 1, cuts, dp) +
+                       solve(k + 1, j, cuts, dp);
+
+            mini = min(mini, cost);
+        }
+        return dp[i][j] = mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        sort(cuts.begin(), cuts.end());
+        int c = cuts.size() - 2;
+        return solve(1, c, cuts, dp);
+    }
+};
+
+// Tabulation
+
