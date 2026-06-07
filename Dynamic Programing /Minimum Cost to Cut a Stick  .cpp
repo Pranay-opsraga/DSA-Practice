7547c53fb2e5cd5afe82b7dd6dc84e80333db5be
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-07 19:18:46 +0530

    Implement tabulation for minimum cost to cut a stick

--- Dynamic Programing /Minimum Cost to Cut a Stick  .cpp
+++ Dynamic Programing /Minimum Cost to Cut a Stick  .cpp
@@ -30,3 +30,28 @@ public:
 
 // Tabulation
 
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+
+        sort(cuts.begin(), cuts.end());
+        int c = cuts.size() - 2;
+        vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+
+        for (int i = c; i >= 1; i--) {
+            for (int j = i; j <= c; j++) {
+                int mini = 1e9;
+                for (int k = i; k <= j; k++) {
+                    int cost = (cuts[j + 1] - cuts[i - 1]) + dp[i][k - 1] +
+                               dp[k + 1][j];
+
+                    mini = min(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
+    }
+};
