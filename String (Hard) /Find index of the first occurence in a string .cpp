de676efa5ac57907455a2ba8529c4c1c303019e6
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-15 16:53:54 +0530

    Implement strStr function to find substring index

---
+++ String (Hard) /Find index of the first occurence in a string .cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+         
+         if(haystack.size() < needle.size()) return -1;
+         
+         int n = haystack.size();
+         int m = needle.size();
+
+         for(int i = 0 ; i <= n - m ; i++){
+              if(haystack.substr(i , m) == needle){
+                    return i;
+              }
+         }
+         return -1;
+    }
+};
